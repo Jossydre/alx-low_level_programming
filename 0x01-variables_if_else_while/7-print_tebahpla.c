c0a8eaa12f728877708c6f38ffaf08449978d184
Jossydre <makinwadarejossy@gmail.com>
2023-08-12 08:25:55 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints the lowercase alphabet in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char d;
+
+	d = 'z';
+	while
+		(d >= 'a') {
+			putchar(d);
+			d--;
+		}
+	putchar('\n');
+	return (0);
+}
+
