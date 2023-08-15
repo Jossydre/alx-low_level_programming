7d71b4b773ef7f46984f904766bc54599e980f08
Jossydre <makinwadarejossy@gmail.com>
2023-08-15 10:04:38 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,18 +5,18 @@
  */
 void print_alphabet_x10(void)
 {
-	char d;
-	int i;
-	i = 1;
+char d;
+int i;
 
-	{
-		for (i = 1; i <= 10 ; i++)
-		{
-			for (d = 'a'; d <= 'z'; d++)
-			{
-				_putchar(d);
-			}
-			_putchar('\n');
-		}
-	}
+i = 1;
+{
+for (i = 1; i <= 10; i++)
+{
+for (d = 'a'; d <= 'z'; d++)
+{
+_putchar(d);
+}
+_putchar('\n');
+}
+}
 }
