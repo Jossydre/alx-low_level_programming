7be970f542103a093ec8e7c0d64db82f53f5ab7d
Jossydre <makinwadarejossy@gmail.com>
2023-08-16 09:08:20 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - computes and prints the sum of all the multiples
+ * Return: 0
+ */
+int main(void)
+{
+	int sum, num;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0))
+			sum += num;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
