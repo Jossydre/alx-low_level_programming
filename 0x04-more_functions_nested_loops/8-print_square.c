7fce8eecfdc911538fe0f273317955c66c532f79
Jossydre <makinwadarejossy@gmail.com>
2023-08-18 06:18:58 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,7 +10,7 @@ void print_square(int size)
 
 	for (row = 1 ; row <= size ; row++)
 	{
-		for (column = 1 ; column <= size ; size++)
+		for (column = 1 ; column <= size ; column++)
 			_putchar('#');
 		_putchar('\n');
 	}
