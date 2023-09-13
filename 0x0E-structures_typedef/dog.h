075f9b1e05d229fc05045708ec21d3fd386eaeb1
Jossydre <makinwadarejossy@gmail.com>
2023-09-13 06:34:21 -0700

    New name

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,6 +9,12 @@
  *
  * Decription: longer description
  */
+
+/**
+ * dog_t - new name for struct dog
+ */
+typedef struct dog dog_t;
+
 struct dog
 {
 	char *name;
@@ -18,5 +24,6 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
