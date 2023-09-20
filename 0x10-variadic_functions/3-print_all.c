5904009d170c42d81b6db6b9fd6ad8fbfe482472
Jossydre <makinwadarejossy@gmail.com>
2023-09-20 07:08:15 -0700

    value

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,49 +5,48 @@
 /**
  * print_all - prints anything.
  * @format: a list of types of arguments passed to the function.
- *
  * Return: no return.
  */
 void print_all(const char * const format, ...)
 {
-	va_list dre;
-	unsigned int n = 0, m, o = 0;
+	va_list valist;
+	unsigned int i = 0, j, c = 0;
 	char *str;
-	const char d_arg[] = "cifs";
+	const char t_arg[] = "cifs";
 
-	va_start(dre, format);
-	while (format && format[n])
+	va_start(valist, format);
+	while (format && format[i])
 	{
-		m = 0;
-		while (d_arg[m])
+		j = 0;
+		while (t_arg[j])
 		{
-			if (format[n] == d_arg[m] && o)
+			if (format[i] == t_arg[j] && c)
 			{
 				printf(", ");
 				break;
-			} m++;
+			} j++;
 		}
-		switch (format[n])
+		switch (format[i])
 		{
-			case 'c':
-				printf("%c", va_arg(dre, int)), o = 1;
+		case 'c':
+			printf("%c", va_arg(valist, int)), c = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(valist, int)), c = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(valist, double)), c = 1;
+			break;
+		case 's':
+			str = va_arg(valist, char *), c = 1;
+			if (!str)
+			{
+				printf("(nil)");
 				break;
-			case 'i':
-				printf("%d", va_arg(dre, int)), o = 1;
-				break;
-			case 'f':
-				printf("%f", va_arg(dre, double)), o = 1;
-				break;
-			case 's':
-				str = va_arg(dre, char *), o = 1;
-				if (!str)
-				{
-					printf("(nil)");
-					break;
-				}
-				printf("%s", str);
-				break;
-		} m++;
+			}
+			printf("%s", str);
+			break;
+		} i++;
 	}
-	printf("\n"), va_end(dre);
+	printf("\n"), va_end(valist);
 }
